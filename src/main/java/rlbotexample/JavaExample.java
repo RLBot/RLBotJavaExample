d4a5d666ce86e816cdcacb02ed445f3e217321fb
Tyler Arehart <tarehart@gmail.com>
2019-06-11 09:42:54 -0700

    Switching to socket-based python interop, and adding a nice icon.

--- src/main/java/rlbotexample/JavaExample.java
+++ src/main/java/rlbotexample/JavaExample.java
@@ -1,15 +1,15 @@
 package rlbotexample;
 
 import rlbot.manager.BotManager;
-import rlbot.pyinterop.PythonInterface;
-import rlbot.pyinterop.PythonServer;
 import rlbotexample.util.PortReader;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.awt.event.ActionListener;
-import java.util.OptionalInt;
+import java.net.URL;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * See JavaAgent.py for usage instructions.
@@ -18,41 +18,57 @@ import java.util.Set;
  */
 public class JavaExample {
 
+    private static final Integer DEFAULT_PORT = 17357;
+
     public static void main(String[] args) {
 
         BotManager botManager = new BotManager();
-        PythonInterface pythonInterface = new SamplePythonInterface(botManager);
-        Integer port = PortReader.readPortFromFile("port.cfg");
-        PythonServer pythonServer = new PythonServer(pythonInterface, port);
-        pythonServer.start();
+        Integer port = PortReader.readPortFromArgs(args).orElseGet(() -> {
+            System.out.println("Could not read port from args, using default!");
+            return DEFAULT_PORT;
+        });
+
+        SamplePythonInterface pythonInterface = new SamplePythonInterface(port, botManager);
+        new Thread(pythonInterface::start).start();
 
         JFrame frame = new JFrame("Java Bot");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
         panel.setBorder(new EmptyBorder(10, 10, 10, 10));
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.add(new JLabel("Listening on port " + port));
-        panel.add(new JLabel("I'm the thing controlling the Java bot, keep me open :)"));
+        BorderLayout borderLayout = new BorderLayout();
+        panel.setLayout(borderLayout);
+        JPanel dataPanel = new JPanel();
+        dataPanel.setLayout(new BoxLayout(dataPanel, BoxLayout.Y_AXIS));
+        dataPanel.setBorder(new EmptyBorder(0, 10, 0, 0));
+        dataPanel.add(new JLabel("Listening on port " + port), BorderLayout.CENTER);
+        dataPanel.add(new JLabel("I'm the thing controlling the Java bot, keep me open :)"), BorderLayout.CENTER);
         JLabel botsRunning = new JLabel("Bots running: ");
-        panel.add(botsRunning);
+        dataPanel.add(botsRunning, BorderLayout.CENTER);
+        panel.add(dataPanel, BorderLayout.CENTER);
         frame.add(panel);
+
+        URL url = JavaExample.class.getClassLoader().getResource("icon.png");
+        Image image = Toolkit.getDefaultToolkit().createImage(url);
+        panel.add(new JLabel(new ImageIcon(image)), BorderLayout.WEST);
+        frame.setIconImage(image);
 
         frame.pack();
         frame.setVisible(true);
 
         ActionListener myListener = e -> {
             Set<Integer> runningBotIndices = botManager.getRunningBotIndices();
-            OptionalInt maxIndex = runningBotIndices.stream().mapToInt(k -> k).max();
-            String botsStr = "None";
-            if (maxIndex.isPresent()) {
-                StringBuilder botsStrBuilder = new StringBuilder();
-                for (int i = 0; i <= maxIndex.getAsInt(); i++) {
-                    botsStrBuilder.append(runningBotIndices.contains(i) ? "☑ " : "☐ ");
-                }
-                botsStr = botsStrBuilder.toString();
+
+            String botsStr;
+            if (runningBotIndices.isEmpty()) {
+                botsStr = "None";
+            } else {
+                botsStr = runningBotIndices.stream()
+                        .sorted()
+                        .map(i -> "#" + i)
+                        .collect(Collectors.joining(", "));
             }
-            botsRunning.setText("Bots running: " + botsStr);
+            botsRunning.setText("Bots indices running: " + botsStr);
         };
 
         new Timer(1000, myListener).start();
--- src/main/python/javaExample.py
+++ src/main/python/javaExample.py
@@ -1,18 +1,15 @@
-import os
-
 from rlbot.agents.base_agent import BOT_CONFIG_AGENT_HEADER
-from rlbot.agents.base_java_agent import BaseJavaAgent
+from rlbot.agents.executable_with_socket_agent import ExecutableWithSocketAgent
 from rlbot.parsing.custom_config import ConfigHeader, ConfigObject
 
 
-class JavaExample(BaseJavaAgent):
-    def get_port_file_path(self):
-        # Look for a port.cfg file in the same directory as THIS python file.
-        return os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), 'port.cfg'))
+class JavaExample(ExecutableWithSocketAgent):
+    def get_port(self) -> int:
+        return 17357
 
     def load_config(self, config_header: ConfigHeader):
-        self.java_executable_path = config_header.getpath('java_executable_path')
-        self.logger.info("Java executable is configured as {}".format(self.java_executable_path))
+        self.executable_path = config_header.getpath('java_executable_path')
+        self.logger.info("Java executable is configured as {}".format(self.executable_path))
 
     @staticmethod
     def create_agent_configurations(config: ConfigObject):
